b22e6c813924270e072e010e3428fe1f217ef151
Davide Schiaroli <davide.schiaroli2@studio.unibo.it>
2019-10-11 14:28:27 +0200

    Initial code, defined component pin, added pinmode on used pins, level managing added

---
+++ progetto1_IOT.ino
@@ -0,0 +1,36 @@
+#define LED_VERDE_1 2
+#define LED_VERDE_2 5 
+#define LED_VERDE_3 7
+#define LED_BIANCO 9
+#define LED_ROSSO 8
+#define POTENTIOMETER A0
+#define BUTTON_START 12
+#define BUTTON_DOWN 13
+
+int livello = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_VERDE_1,OUTPUT);
+  pinMode(LED_VERDE_2,OUTPUT);
+  pinMode(LED_VERDE_3,OUTPUT);
+  pinMode(LED_ROSSO,OUTPUT);
+  pinMode(LED_BIANCO,OUTPUT);
+  pinMode(POTENTIOMETER,INPUT);
+  pinMode(BUTTON_START,INPUT);
+  pinMode(BUTTON_DOWN,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  /*
+   * Leggo il potenziometro e scelgo il livello
+   */
+  livello=scegli_livello(analogRead(POTENTIOMETER));
+}
+
+int scegli_livello(int valore){
+  return valore/128;
+}
