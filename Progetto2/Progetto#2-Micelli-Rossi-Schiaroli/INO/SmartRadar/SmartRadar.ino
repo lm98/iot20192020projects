c45652a759eb78f71de1d83f72545815ee0473ec
Davide Schiaroli <davide.schiaroli2@studio.unibo.it>
2019-11-17 16:53:01 +0100

    Added flow control and reading from serial, need task part to complete receiving

--- Progetto2/Progetto#2-Micelli-Rossi-Schiaroli/INO/SmartRadar/SmartRadar.ino
+++ Progetto2/Progetto#2-Micelli-Rossi-Schiaroli/INO/SmartRadar/SmartRadar.ino
@@ -1,24 +1,130 @@
 #include "Scheduler.h"
 #include "SlowBlink.h"
 
-
 #define RX 0
 #define TX 1
 
 Scheduler scheduler;
 
-void setup() {
+String inputString = "";     // a String to hold incoming data
+bool stringComplete = false; // whether the string is complete
+int param;
+int servoSpeed = 0;
+
+enum service_type
+{
+  SINGLE,
+  MANUAL,
+  AUTO
+};
+
+void setup()
+{
   Serial.begin(9600);
   scheduler.init(100);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
 
-  Task* t0 = new SlowBlink(13);
+  MsgService.init();
+  Task *t0 = new SlowBlink(13);
   t0->init(100);
   scheduler.addTask(t0);
 }
 
-void loop() {
-  if (Serial.available() > 0){
-    scheduler.schedule();  
+void loop()
+{
+  scheduler.schedule();
+  service_type state = MANUAL;
+
+  if (stringComplete)
+  {
+    //Serial.println(inputString);
+    inputString = ""; // to clear string
+    stringComplete = false;
   }
-	
+
+  //Leo non piangere quando vedrai questo:
+  switch (state)
+  {
+
+  case SINGLE:
+    servoSpeed = param;
+    break;
+
+  case MANUAL:
+    //Move to direction <param>
+    break;
+
+  case AUTO:
+    servoSpeed = param;
+    break;
+
+  default:
+
+    break;
+  }
+}
+/* 
+  Serial communication rules:
+
+  Manual message param:
+    Sending: <direction> <isTakingOver> <distance>
+      The first is the radius between the sonar and 0, it is a value between 0 and 180 more or less
+      The second is a boolean indicating if the sonar is taking over any object
+      The third is the distance between object and the sonar, if it is negative indicate no object 
+    Receive: <direction>
+
+  Single message param:
+    Sending: <angle> and <distance>
+      For each object identified, i send <angle> and <distance>
+    Receiving: <Value>
+      User can change servo speed with specified value
+
+  Auto message param:
+    Sending: <angle> <distance>
+      or <alarm> <value>
+      value can be alarmTrue/False
+      or TrackingTrue/False
+      or TrackingTrue + distance
+    Receiving: <Value>
+      User can change servo speed with specified value
+*/
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+
+    switch (inChar)
+    {
+    case 's':
+      state = SINGLE;
+      break;
+    case 'm':
+      state = MANUAL;
+      break;
+    case 'a':
+      state = AUTO;
+      break;
+    case '/n':
+      stringComplete = true;
+      break;
+    default: // Every digit is a byte
+      param = Serial.parseInt(' '))
+      if(param==0)
+      {
+        Serial.print("Error reading from stream");
+      }
+      break;
+    }
+  }
 }
