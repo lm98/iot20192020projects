e986470b67bc89541c2016c5ee5a137f14595ce6
davide <davide.schiaroli2@studio.unibo.it>
2020-05-14 15:11:53 +0200

    Added led managemente, modified json

--- Progetto3/esp/esp.ino
+++ Progetto3/esp/esp.ino
@@ -1,6 +1,8 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
+#define LED_AV 16
+#define LED_NAV 5
 /* wifi network name */
 //char* ssidName = "FASTWEB-enzo-2,4";
 char* ssidName = "Wifi";
@@ -10,7 +12,11 @@ char* pwd = "password";
 /* service IP address */ 
 char* address = "http://allco.000webhostapp.com";
 
-void setup() { 
+int flag = 0;
+
+void setup() {
+  pinMode(LED_AV, OUTPUT);    //Dichiaro il pin D0 "AVAILABLE" in Output
+  pinMode(LED_NAV, OUTPUT);    //Dichiaro il pin D1 "NOT AVAILABLE" in Output
   Serial.begin(115200);                                
   WiFi.begin(ssidName, pwd);
   Serial.print("Connecting...");
@@ -26,25 +32,22 @@ int sendData(String address, float value, int type){
    http.begin(address + "/dumpster/insert_weight.php");      
    http.addHeader("Content-Type", "application/json");
    String msg = "";
-   
-   switch(type){
-    case 1:
-      msg = "{ \"a\":"+ String(value) +",\"b\":" + String(0) + ",\"c\":" + String(0) +"}";
-      break;
-     case 2:
-      msg = "{ \"a\":"+ String(0) +",\"b\":" + String(value) + ",\"c\":" + String(0) +"}";
-      break;
-     case 3:
-      msg = "{ \"a\":"+ String(0) +",\"b\":" + String(0) + ",\"c\":" + String(value) +"}";
-      break;
-     default:
-      msg = "Error on creating string";
-      break;
+
+   if ( value >= 95 && flag == 0) { //SET DUMPSTER NOT AVAILABLE
+    flag = 1;
+    msg = "{avail:0,pass:pass}";
+    setNAval(); //Turn on leds
    }
-    
+   else if (flag == 1){ //SET DUMPSTER AVAILABLE
+    flag = 0;
+    msg = "{avail:1,pass:pass}";
+    setAval(); //Turn on leds
+   }else{
+    msg = "{ \"quantity\":"+ String(value) + "}"; 
+   }
+   
    int retCode = http.POST(msg);   
-   http.end();  
-      
+   http.end();
    //String payload = http.getString();  
    //Serial.println(payload);      
    return retCode;
@@ -53,8 +56,8 @@ int sendData(String address, float value, int type){
 void loop() { 
  if (WiFi.status()== WL_CONNECTED){   
 
-   /* read sensor */
-   float value = (float) analogRead(A0) *100 / 1023.0;
+   /* read sensor from 0 to 100 kilos */
+   float value = (float) analogRead(A0) * 100 / 1023.0;
    
    /* send data */
    Serial.print("sending " + String(value) + "...");
@@ -75,4 +78,15 @@ void loop() {
  
  delay(5000);  
  
+}
+
+
+void setAval(){
+  digitalWrite(LED_AV,HIGH);
+  digitalWrite(LED_NAV,LOW);
+}
+
+void setNAval(){
+  digitalWrite(LED_NAV,HIGH);
+  digitalWrite(LED_AV,LOW);
 }
