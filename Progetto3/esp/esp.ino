b17ce529a0fd68c0eb1bfeb2b62c500df0e43600
davide <davide.schiaroli2@studio.unibo.it>
2020-05-17 14:11:02 +0200

    Modified esp management of available state

--- Progetto3/esp/esp.ino
+++ Progetto3/esp/esp.ino
@@ -15,37 +15,40 @@ char* pwd = "password";
 /* service IP address */ 
 char* address = "http://allco.000webhostapp.com";
 
+HTTPClient http;
 int flag = 0;
 float value = 0;
 float oldvalue = 0;
 
-
-int sendData(String address, float value) {  
-   HTTPClient http;    
-   String msg = "";
-
-   if ( value >= 95 && flag == 0) { //SET DUMPSTER NOT AVAILABLE
-    http.begin(address + "/dumpster/set_availability_esp.php");
-    Serial.print("Setting dumpster not available");
-    flag = 1;
-    msg = "{avail:0,pass:password}";
-    setNAval(); //Turn on leds
-   }
-   else if (flag == 1){ //SET DUMPSTER AVAILABLE
-    http.begin(address + "/dumpster/set_availability_esp.php");
-    Serial.print("Setting dumpster available");
-    flag = 0;
-    msg = "{avail:1,pass:password}";
-    setAval(); //Turn on leds
-   }else{ //SEND WEIGHT
-    http.begin(address + "/dumpster/insert_weight.php");
-    msg = "{ \"quantity\":"+ String(value) + "}"; 
-   }
-   
+int sendData(float value) {      
+   String msg = "{ \"quantity\":"+ String(value) + "}";
+   http.begin(address + String("/dumpster/insert_weight.php"));
    http.addHeader("Content-Type", "application/json");
    int retCode = http.POST(msg);   
-   http.end();    
+   http.end();
+   if (value >= 95){  
+    setNAvail();
+    postNotAvail();
+   }    
    return retCode;
+}
+
+int isAvailable() {     
+   http.begin(address + String("/dumpster/permission.php"));
+   http.addHeader("Content-Type", "application/json");
+   int retCode = http.POST("");   
+   http.end();    
+   if(retCode == 200){
+    setAvail();
+    flag = 1;
+   } else if (retCode == 418) {
+    setNAvail();
+    flag = 0;
+   } else {
+    Serial.println("Error asking dumpster state");
+   }
+   
+  return flag;
 }
 
 void setup() {
@@ -58,43 +61,55 @@ void setup() {
     delay(500);
     Serial.print(".");
   } 
-  Serial.println("Connected: \n local IP: " + WiFi.localIP());
-  setAval();
+  Serial.println("\nConnected: \n local IP: " + WiFi.localIP());
+  if(isAvailable()){
+    setAvail();
+  } else {
+    setNAvail();
+  }
 }
    
 void loop() { 
  if (WiFi.status()== WL_CONNECTED){   
-  
+    
    value = (float) analogRead(A0) * 100 / 1023.0; //read sensor from 0 to 100 kilos
    
-   if((value < oldvalue - 3 || value > oldvalue + 3) && value -oldvalue  > 2 ){ //Control if the weight is changed with a threshold of 3 kilos
+   if((value < oldvalue - 3 || value > oldvalue + 3) && value > 3) { //Control if the weight is changed with a threshold of 3 kilos
 
     Serial.print("sending " + String(value - oldvalue) + "..."); //Print value sent
-   
-    int code = sendData(address, value); //send data
-
-    /* log result */
+    if(isAvailable()){
+    
+    int code = sendData(value - oldvalue); //send data
+       
     if (code == 200){
       Serial.println("ok");   
     } else {
       Serial.println("error");
-    }
+    } 
+   } 
   }
   oldvalue = value;
   delay(5000);  
- }
- else { 
+ } else { 
    Serial.println("Error in WiFi connection");   
  }
 } 
 
-
-void setAval(){
+void setAvail(){
   digitalWrite(LED_AV,HIGH);
   digitalWrite(LED_NAV,LOW);
 }
 
-void setNAval(){
+void setNAvail(){
   digitalWrite(LED_NAV,HIGH);
-  digitalWrite(LED_AV,LOW);
+  digitalWrite(LED_AV,LOW); 
+}
+
+int postNotAvail(){    
+   String msg = "{avail:0,pass:password}";
+   http.begin(address + String("/dumpster/set_availability_esp.php"));
+   http.addHeader("Content-Type", "application/json");
+   int retCode = http.POST(msg);   
+   http.end();    
+   return retCode;
 }
